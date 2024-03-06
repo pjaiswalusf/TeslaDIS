862c7144d14e0273e6bb125809df9bc30e1b765e
senjapuri <159227703+senjapuri@users.noreply.github.com>
2024-03-06 15:48:20 -0500

    Add files via upload
    Added Engine Class

---
+++ Tesla/Tesla/Engine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesla
+{
+    internal class Engine
+    {
+        public string EngineType;
+        public string Horsepower;
+        public Engine()
+        {
+            EngineType = "3000AMP";
+            Horsepower = "80HP";
+        }
+
+        public string EngineDetails()
+        {
+            return EngineType + ' ' + Horsepower;
+        }
+    }
+}
