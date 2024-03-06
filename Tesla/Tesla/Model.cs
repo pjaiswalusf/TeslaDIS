4288dfb458abb705f43157f8ef1f5dbaa03a24a6
arpanjaiswal <142117565+arpanjaiswal@users.noreply.github.com>
2024-03-06 15:48:18 -0500

    Add files via upload
    Wheels:Model

---
+++ Tesla/Tesla/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesla
+{
+    internal class Model
+    {
+        public static string ModelName;
+        public static string ModelVersion;
+        public int ModelPrice;
+        public Model()
+        {
+            ModelName = "Model X";
+            ModelVersion = "1.0";
+            ModelPrice = 35000;
+        }
+
+        public Model(string modelName, string modelVersion,int modelPrice)
+        {
+            ModelName = modelName;
+            ModelVersion = modelVersion;
+            this.ModelPrice = modelPrice;
+        }
+    }
+}
