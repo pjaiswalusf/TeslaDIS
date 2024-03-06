e4720728275635f527b87d1f8e7d285b301e6839
Pratik Jaiswal <143289852+pjaiswalusf@users.noreply.github.com>
2024-03-06 16:12:32 -0500

    Add files via upload

--- Tesla/Tesla/Program.cs
+++ Tesla/Tesla/Program.cs
@@ -4,9 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        ModelType Type = new ModelType();
+        Console.WriteLine("The Model Name and year are: " + Type.getModel());
+
+        Engine engine1 = new Engine();
+        Console.WriteLine("Tesla Engine and Horsepower details are: " + engine1.EngineDetails());
+
         Instructions instr = new Instructions();
         Console.WriteLine("Seat belt instructions: " + instr.seatBelt() +
             " | Speed Limit Instruction:  " + instr.speedLimit());
+
+        Model model = new Model();
+        Console.WriteLine(model.getModel());
+
+        Wheels wheel1 = new Wheels();
+        Console.WriteLine("The wheel size is: " + wheel1.getWheelSize());
     }
 }
 
